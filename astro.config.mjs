9d72d4256863b6e2f230e5338bf179315b9d8bd1
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-12-15 00:11:33 +0900

    config 修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(
+          path.dirname(fileURLToPath(import.meta.url)),
+          "./src"
+        ),
+      },
+    },
+  },
 });
