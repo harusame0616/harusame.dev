73587fc2d61cf5043cc5f26b9bc1dbb28d952a72
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-12-23 16:00:14 +0900

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,20 @@ import tailwind from "@astrojs/tailwind";
 import path from "path";
 import { fileURLToPath } from "url";
 import react from "@astrojs/react";
-
 import prefetch from "@astrojs/prefetch";
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), prefetch()],
+  integrations: [tailwind(), react(), prefetch(), sitemap()],
+  site: "https://tecparty.harusame.dev",
   vite: {
     resolve: {
       alias: {
-        "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src")
-      }
-    }
-  }
+        "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+      },
+    },
+  },
 });
