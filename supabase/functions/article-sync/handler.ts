da139bde2389f6363e974748b3467befd32f658e
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-06-27 09:05:10 +0900

    ハンドラ実装

---
+++ supabase/functions/article-sync/handler.ts
@@ -0,0 +1,37 @@
+import {
+    ArticleSyncUsecase,
+    MicroCmsArticleGateway,
+    SupabaseArticleRepository,
+} from "article";
+import { createClient as createSupabaseClient } from "npm:@supabase/supabase-js";
+import { createClient as createMicroCMSClient } from "npm:microcms-js-sdk";
+import { getConfig } from "./get-config.ts";
+
+export async function handler(req: Request) {
+    const config = getConfig();
+
+    if (req.headers.get("Authorization") !== `Bearer ${config.auth.secret}`) {
+        return new Response(null, { status: 401 });
+    }
+
+    const articleSyncUsecase = new ArticleSyncUsecase(
+        new MicroCmsArticleGateway(
+            createMicroCMSClient({
+                serviceDomain: config.microCMS.serviceDomain,
+                apiKey: config.microCMS.apiKey,
+            }),
+        ),
+        new SupabaseArticleRepository(
+            createSupabaseClient(
+                config.supabase.url,
+                config.supabase.serviceRoleKey,
+            ),
+        ),
+    );
+
+    const result = await articleSyncUsecase.execute();
+
+    return result.success
+        ? new Response()
+        : Response.json(result, { status: 500 });
+}
