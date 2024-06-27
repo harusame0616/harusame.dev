462fb98cdab17150b49ec803284b485260bd1f1e
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-06-27 09:04:15 +0900

    記事同期処理実装

---
+++ src/features/article/infrastructures/microcms-article-gateway.ts
@@ -0,0 +1,23 @@
+import { getLogger } from "@/lib/logger/get-logger";
+import { fail, succeed, type Result } from "@/lib/result";
+import { type ArticleGateway } from "@features/article/usecases/article-gateway";
+import { createClient } from "microcms-js-sdk";
+
+export class MicroCmsArticleGateway implements ArticleGateway {
+  constructor(private microcms: ReturnType<typeof createClient>) {}
+
+  async getAllArticleSlugs(): Promise<Result<string[]>> {
+    try {
+      const res = await this.microcms.getAllContentIds({
+        endpoint: "blogs",
+      });
+
+      return succeed(res);
+    } catch (error) {
+      const logger = getLogger();
+      logger.error(JSON.stringify({ error }));
+
+      return fail("記事の取得に失敗しました");
+    }
+  }
+}
