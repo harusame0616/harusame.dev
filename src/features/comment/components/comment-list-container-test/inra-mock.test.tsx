085bc5a7e166636db3aa621015415e7c97405f16
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-07-03 20:54:27 +0900

    テスト修正

---
+++ src/features/comment/components/comment-list-container-test/inra-mock.test.tsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
+
+import { SWRConfig } from "swr";
+import { CommentListContainer } from "../comment-list-container";
+
+const mocks = vi.hoisted(() => ({
+  queryComments: vi.fn(),
+}));
+
+vi.mock("../infrastractures/get-comments", () => ({
+  queryComments: mocks.queryComments,
+}));
+
+test("読込中のときにローディングが表示される", () => {
+  const screen = render(
+    <SWRConfig value={{ dedupingInterval: 0 }}>
+      <CommentListContainer articleSlug="test" />
+    </SWRConfig>
+  );
+  const loadingItem = screen.getByRole("listitem", { name: "読込中" });
+  expect(loadingItem).toBeVisible();
+});
