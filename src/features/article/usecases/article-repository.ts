62ee884ec26e9babf9697e8b985bfc15a86aa143
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-07-09 09:24:15 +0900

    通知実装

--- src/features/article/usecases/article-repository.ts
+++ src/features/article/usecases/article-repository.ts
@@ -2,6 +2,7 @@ import type { Result } from "@/lib/result";
 import type { Article } from "@features/article/models/article";
 
 export interface ArticleRepository {
+  getByArticleId(articleId: string): Promise<Result<Article>>;
   listAll(): Promise<Result<Article[]>>;
   saveMany(articles: Article[]): Promise<Result>;
   deleteManyBySlug(slugs: string[]): Promise<Result>;
---
+++ src/features/comment/usecases/notification-gateway.ts
@@ -0,0 +1,3 @@
+export interface NotificationGateway {
+    notify(text: string): Promise<void>;
+}
