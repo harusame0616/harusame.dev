1c8a70f33b7b592d5f47d8df96c46ab771770402
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-07-17 08:41:41 +0900

    メニュー改善

---
+++ src/components/menu/menu.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const menuIds = ["top", "blog", "profile"] as const;
+export type Menu = typeof menuIds[number];
+
+export const menus = [
+  { "href": "/", "label": "トップ", "id": "top" },
+  { "href": "/blog/pages/1/", "label": "ブログ", "id": "blog" },
+  { "href": "/profile/", "label": "プロフィール", "id": "profile" },
+] as const satisfies Array<{ href: string; label: string; id: Menu }>;
+
+export function parseMenu(v: unknown): Menu {
+  return z.enum(menuIds).parse(v);
+}
