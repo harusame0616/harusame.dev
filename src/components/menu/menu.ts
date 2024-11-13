e71b6c3da5c0939caaa58a0820b6e64225d8f3cf
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2024-11-13 21:57:28 +0900

    プロダクト追加 (#40)

--- src/components/menu/menu.ts
+++ src/components/menu/menu.ts
@@ -1,11 +1,12 @@
 import z from "zod";
 
-export const menuIds = ["top", "blog", "profile"] as const;
+export const menuIds = ["top", "blog", "profile", "products"] as const;
 export type Menu = (typeof menuIds)[number];
 
 export const menus = [
   { href: "/", label: "トップ", id: "top" },
   { href: "/blog/pages/1/", label: "ブログ", id: "blog" },
+  { href: "/products/", label: "プロダクト", id: "products" },
   { href: "/profile/", label: "プロフィール", id: "profile" },
 ] as const satisfies Array<{ href: string; label: string; id: Menu }>;
 
