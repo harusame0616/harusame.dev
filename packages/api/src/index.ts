693914ae9988239ce7171aacf3bffd0c1d78ecd9
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-01-26 09:17:47 +0900

    パスチェック

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,18 +1,18 @@
 import * as v from "valibot";
 
 export default {
-	async fetch(request, env): Promise<Response> {
-		const { DATABASE } = env;
+	async fetch(request, { DATABASE }): Promise<Response> {
+		const url = new URL(request.url);
+
+		const [, articleId] =
+			/\/articles\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})\/comments/
+				.exec(url.pathname.toLowerCase()) || [];
+
+		if (!articleId) {
+			return new Response("bad request", { status: 400 });
+		}
+
 		if (request.method === "GET") {
-			const url = new URL(request.url);
-			const paths = url.pathname.split("/");
-			const articleId = paths[1];
-			console.log({ articleId });
-
-			if (!articleId) {
-				return new Response(`bad request${articleId}`, { status: 400 });
-			}
-
 			const stmt = DATABASE.prepare(
 				"SELECT * FROM comment WHERE article_id = ?",
 			);
@@ -20,6 +20,7 @@ export default {
 			if (!queryCommentsResult.success) {
 				return new Response("server error", { status: 500 });
 			}
+
 			return Response.json(
 				{
 					comments: queryCommentsResult.results.map((comment) => ({
@@ -34,18 +35,16 @@ export default {
 		}
 
 		if (request.method === "POST") {
-			console.log("post");
-			const body = await request.json();
-			const parseResult = v.safeParse(
+			const paramsResult = v.safeParse(
 				v.object({
 					articleId: v.string(),
 					name: v.string(),
 					text: v.string(),
 				}),
-				body,
+				await request.json(),
 			);
 
-			if (!parseResult.success) {
+			if (!paramsResult.success) {
 				return new Response("bad request", { status: 400 });
 			}
 
@@ -54,15 +53,15 @@ export default {
 			);
 			const result = await stmt.bind(
 				crypto.randomUUID(),
-				parseResult.output.articleId,
-				parseResult.output.name,
-				parseResult.output.text,
+				paramsResult.output.articleId,
+				paramsResult.output.name,
+				paramsResult.output.text,
 				new Date().toISOString(),
 			).run();
 
-			console.log({ result });
+			return new Response("ok", { status: 200 });
 		}
 
-		return new Response("ok", { status: 200 });
+		return new Response("method not allowed", { status: 405 });
 	},
 } satisfies ExportedHandler<Env>;
