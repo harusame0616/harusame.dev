758c02bcf0e0f2ed9f9756376da752885d4ac6ba
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-01-26 09:21:15 +0900

    biome

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -5,8 +5,9 @@ export default {
 		const url = new URL(request.url);
 
 		const [, articleId] =
-			/\/articles\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})\/comments/
-				.exec(url.pathname.toLowerCase()) || [];
+			/\/articles\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})\/comments/.exec(
+				url.pathname.toLowerCase(),
+			) || [];
 
 		if (!articleId) {
 			return new Response("bad request", { status: 400 });
@@ -21,17 +22,15 @@ export default {
 				return new Response("server error", { status: 500 });
 			}
 
-			return Response.json(
-				{
-					comments: queryCommentsResult.results.map((comment) => ({
-						articleId: comment.article_id,
-						commentId: comment.comment_id,
-						name: comment.name,
-						text: comment.text,
-						commentedAt: comment.commented_at,
-					})),
-				},
-			);
+			return Response.json({
+				comments: queryCommentsResult.results.map((comment) => ({
+					articleId: comment.article_id,
+					commentId: comment.comment_id,
+					name: comment.name,
+					text: comment.text,
+					commentedAt: comment.commented_at,
+				})),
+			});
 		}
 
 		if (request.method === "POST") {
@@ -51,13 +50,15 @@ export default {
 			const stmt = DATABASE.prepare(
 				"INSERT INTO comment (comment_id, article_id, name, text, commented_at) VALUES (?, ?, ?, ?, ?)",
 			);
-			const result = await stmt.bind(
-				crypto.randomUUID(),
-				paramsResult.output.articleId,
-				paramsResult.output.name,
-				paramsResult.output.text,
-				new Date().toISOString(),
-			).run();
+			const result = await stmt
+				.bind(
+					crypto.randomUUID(),
+					paramsResult.output.articleId,
+					paramsResult.output.name,
+					paramsResult.output.text,
+					new Date().toISOString(),
+				)
+				.run();
 
 			return new Response("ok", { status: 200 });
 		}
