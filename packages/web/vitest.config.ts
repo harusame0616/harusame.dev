3e83381bbfef4f65b70523eaa1ceb0f4157e5047
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-01-26 21:02:20 +0900

    globals 削除

--- packages/web/vitest.config.ts
+++ packages/web/vitest.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	plugins: [tsconfigPaths()],
 	test: {
-		globals: true,
 		environment: "jsdom",
 		setupFiles: ["./vitest/mock-server.setup.ts"],
 		env: {
