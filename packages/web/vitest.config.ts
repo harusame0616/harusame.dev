9078789e1e84369af0717c565485ad6e96def58f
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2025-01-30 22:03:36 +0900

    astro upgrade (#55)

--- packages/web/vitest.config.ts
+++ packages/web/vitest.config.ts
@@ -1,15 +1,19 @@
+import { defineConfig as defineViteConfig, mergeConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 
-export default defineConfig({
-	plugins: [tsconfigPaths()],
-	test: {
-		environment: "jsdom",
-		setupFiles: ["./vitest/mock-server.setup.ts"],
-		env: {
-			PUBLIC_SUPABASE_URL: "http://127.0.0.1:24321/",
-			PUBLIC_SUPABASE_ANON_KEY:
-				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+export default mergeConfig(
+	defineViteConfig({
+		plugins: [tsconfigPaths()],
+	}),
+	defineVitestConfig({
+		test: {
+			environment: "jsdom",
+			env: {
+				PUBLIC_SUPABASE_URL: "http://127.0.0.1:24321/",
+				PUBLIC_SUPABASE_ANON_KEY:
+					"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+			},
 		},
-	},
-});
+	}),
+);
