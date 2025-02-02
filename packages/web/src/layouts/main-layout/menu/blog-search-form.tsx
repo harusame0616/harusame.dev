fc4932cb114fc32168978011e06bcc6cedd77848
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-02-02 11:17:17 +0900

    searchタグ削除

--- packages/web/src/layouts/main-layout/menu/blog-search-form.tsx
+++ packages/web/src/layouts/main-layout/menu/blog-search-form.tsx
@@ -2,16 +2,14 @@ import { Input } from "@/components/ui/input";
 
 export function BlogSearchForm() {
 	return (
-		<search>
-			<form method="GET" action="https://google.com/search" className="w-full">
-				<input type="hidden" name="q" value="site:https://harusame.dev/" />
-				<Input
-					name="q"
-					type="search"
-					aria-label="検索キーワード"
-					placeholder="検索キーワード"
-				/>
-			</form>
-		</search>
+		<form method="GET" action="https://google.com/search" className="w-full">
+			<input type="hidden" name="q" value="site:https://harusame.dev/" />
+			<Input
+				name="q"
+				type="search"
+				aria-label="検索キーワード"
+				placeholder="検索キーワード"
+			/>
+		</form>
 	);
 }
