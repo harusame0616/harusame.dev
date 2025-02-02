4ee169233e7df07ace2e9ccc0e0e4cea6102be66
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-02-02 11:02:10 +0900

    aria

--- packages/web/src/layouts/main-layout/menu/blog-search-form.tsx
+++ packages/web/src/layouts/main-layout/menu/blog-search-form.tsx
@@ -2,7 +2,7 @@ import { Input } from "@/components/ui/input";
 
 export function BlogSearchForm() {
 	return (
-		<search aria-label="ブログ内検索">
+		<search>
 			<form method="GET" action="https://google.com/search" className="w-full">
 				<input type="hidden" name="q" value="site:https://harusame.dev/" />
 				<Input
