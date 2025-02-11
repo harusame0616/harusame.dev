b6e17d70a5840ceb43080eac13214420466dba79
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2025-02-11 22:18:08 +0900

    blog-client 整理 (#78)
    * blog client 整理
    
    * blogClient 変更箇所対応
    
    * format

--- packages/web/src/features/blog/blog-client.ts
+++ packages/web/src/features/blog/blog-client.ts
@@ -3,7 +3,7 @@ export type Category = {
 	name: string;
 };
 
-export type TagDto = {
+export type Tag = {
 	id: string;
 	name: string;
 };
@@ -14,40 +14,11 @@ export type Post = {
 	title: string;
 	content: string;
 	category: Category;
-	tags: TagDto[];
+	tags: Tag[];
 };
-
-export type PostField = keyof Post;
-
-export type GetPostProps = {
-	id: string;
-};
-
-export type GetPostsProps<U extends PostField> = {
-	count: number;
-	page: number;
-	fields: U[];
-};
-
-export type GetAllPostsProps<U extends PostField> = Omit<
-	GetPostsProps<U>,
-	"count" | "page"
->;
-
-export type GetPostResponse = Promise<Post>;
-
-export type GetPostsResponse<U extends PostField> = Promise<{
-	posts: Pick<Post, U>[];
-	totalCount: number;
-}>;
-
-export type GetAllPostResponse<U extends PostField> = GetPostsResponse<U>;
 
 export interface BlogClient {
-	getPost(props: GetPostProps): GetPostResponse;
-	getPosts<U extends PostField>(props: GetPostsProps<U>): GetPostsResponse<U>;
-	getAllPost<U extends PostField>(
-		props: GetAllPostsProps<U>,
-	): GetAllPostResponse<U>;
-	getAllTags(): Promise<TagDto[]>;
+	getPost(id: string): Promise<Post>;
+	getAllPost(): Promise<Post[]>;
+	getAllTags(): Promise<Tag[]>;
 }
