e5c4ad579371336cfa8ef41efa45b434db0217e5
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-02-16 22:15:46 +0900

    MOCK_BLOG_CLIENT

--- packages/web/src/features/blog/create-blog-client.ts
+++ packages/web/src/features/blog/create-blog-client.ts
@@ -3,7 +3,9 @@ import { MicroCMSBlogClient } from "./microcms-blog-client";
 import { MockBlogClient } from "./mock-blog-client";
 
 export function createBlogClient(): BlogClient {
-	return import.meta.env.MOCK_BLOG_CLIENT === "true"
+	// コマンドラインで MCK_BLOG_CLIENT=true で指定した場合 boolean で .env で指定すると strng のためどちらも指定
+	return import.meta.env.MOCK_BLOG_CLIENT === true ||
+		import.meta.env.MOCK_BLOG_CLIENT === "true"
 		? new MockBlogClient()
 		: new MicroCMSBlogClient({
 				apiKey: import.meta.env.MICROCMS_API_KEY as string,
